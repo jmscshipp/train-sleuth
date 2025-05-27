a4f2487fe7405f4a04073d7e86158243dab408f9
James Shipp <jmscshipp202@gmail.com>
2025-05-27 14:35:48 -0500

    add tunnel generation and collisions on the train!

---
+++ Assets/Scripts/TrainTraveling.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainTraveling : MonoBehaviour
+{
+    [SerializeField]
+    private float travelingSpeed = 10f;
+
+    [SerializeField]
+    private GameObject tunnelSegmentPrefab;
+    [SerializeField]
+    private GameObject tunnelSegmentPrefabLit;
+
+    [SerializeField]
+    private Vector3 frontSegmentStartPos;
+    [SerializeField]
+    private Vector3 midSegmentStartPos;
+    [SerializeField]
+    private Vector3 backSegmentStartPos;
+
+    private Transform frontSegment;
+    private Transform midSegment;
+    private Transform backSegment;
+    private int segmentCounter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        frontSegment = Instantiate(tunnelSegmentPrefab, frontSegmentStartPos, Quaternion.identity).transform;
+        midSegment = Instantiate(tunnelSegmentPrefab, midSegmentStartPos, Quaternion.identity).transform;
+        backSegment = Instantiate(tunnelSegmentPrefab, backSegmentStartPos, Quaternion.identity).transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        frontSegment.transform.Translate(Vector3.forward * travelingSpeed * Time.deltaTime);
+        midSegment.transform.Translate(Vector3.forward * travelingSpeed * Time.deltaTime);
+        backSegment.transform.Translate(Vector3.forward * travelingSpeed * Time.deltaTime);
+
+        // spawn new segment
+        if (frontSegment.transform.position.z > 40f)
+        {
+            // chance to trigger a screenshake
+            if (Random.Range(0f, 1f) > 0.6f)
+                StartCoroutine(ScreenShake());
+
+            Destroy(frontSegment.gameObject);
+            frontSegment = midSegment;
+            midSegment = backSegment;
+            // every fourth segment has lights
+            if (segmentCounter % 4 == 0)
+                backSegment = Instantiate(tunnelSegmentPrefabLit, backSegmentStartPos, Quaternion.identity).transform;
+            else
+                backSegment = Instantiate(tunnelSegmentPrefab, backSegmentStartPos, Quaternion.identity).transform;
+            segmentCounter++;
+        }
+    }
+
+    IEnumerator ScreenShake()
+    {
+        // SOUND EFFECT HERE
+        Vector3 camPos = Camera.main.transform.localPosition;
+        for (int i = 0; i < 5; i++)
+        {
+            Camera.main.transform.localPosition = new Vector3(Camera.main.transform.localPosition.x + Random.insideUnitCircle.x * 0.01f, Camera.main.transform.localPosition.y + Random.insideUnitCircle.y * 0.01f, camPos.z);
+            yield return new WaitForSeconds(0.02f);
+            Camera.main.transform.localPosition = camPos;
+        }
+    }
+}
